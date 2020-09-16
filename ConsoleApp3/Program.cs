452efbc2a3bcb2950fba3f8dcff796477734d194
Melnyk <melnikdmitriy2002@gmail.com>
2020-09-16 13:44:31 +0300

    2nd commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -12,6 +12,20 @@ namespace ConsoleApp3
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine("Привіт Донну і світ");
+            string name = "Dima";
+            int age = 18;
+            bool isEmployed = false;
+            double weight = 68.75;
+
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Возраст: {age}");
+            Console.WriteLine($"Вес: {weight}");
+            Console.WriteLine($"Работает: {isEmployed}");
+
+            Console.WriteLine("Name: {0} Age: {1} Weight: {2} ", name, age, weight);
+
+            Console.WriteLine($"Name: {name} Age: {age} Weight: {weight}");
+
             Console.ReadKey();
         }
     }
